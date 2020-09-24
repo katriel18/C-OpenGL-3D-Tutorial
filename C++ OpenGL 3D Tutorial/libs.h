8298ccb2c7654caf67b274ca4b5974db99430d76
Osti Katriel <katriel181920@gmail.com>
2020-09-23 22:12:57 -0500

    Agregar archivos de proyecto.

---
+++ C++ OpenGL 3D Tutorial/libs.h
@@ -0,0 +1,15 @@
+#pragma once
+#include <iostream>
+
+#include <glfw3.h>
+
+#include <glew.h>
+
+#include <glm.hpp>
+#include <vec2.hpp>
+#include <vec3.hpp>
+#include <vec4.hpp>
+#include <mat4x4.hpp>
+
+#include <gtc/matrix_transform.hpp>
+#include <gtc/type_ptr.hpp>
